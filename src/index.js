0d4f6d9a3156819386004b95ba6b98f7dcd8add5
Glenn Cueto <gcueto@linkedin.com>
2021-06-04 16:44:46 -0700

    add TODO

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+/**
+ * TODO:
+ * - add this expression: let testMetadata = getTestMetadata(this);
+ * - add this expression: testMetadata.filePath = 'string/to/file/path.js';
+ * - add logic to get the actual, current file path
+ * - add logic to check if import already exists
+ */
+
 module.exports = function addMetadata ({
   types: t
 }) {
