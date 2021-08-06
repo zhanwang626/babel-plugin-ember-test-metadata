91f2633834fe9e5f116dada54fc14f610359cde1
Steve Calvert <scalvert@linkedin.com>
2021-08-06 13:46:03 -0700

    chore: Cleanup of file casing, incorrect configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: 'script',
+    sourceType: 'module',
   },
   overrides: [
     {
