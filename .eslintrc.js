0ed1c426028fe62dceda6a60abd9a6f337686244
Steve Calvert <scalvert@linkedin.com>
2021-09-08 14:18:40 -0700

    Resaving eslintrc to fix quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,15 @@ module.exports = {
     node: true,
     es6: true,
   },
-  plugins: ["node", "prettier"],
-  extends: ["eslint:recommended", "plugin:node/recommended", "prettier"],
+  plugins: ['node', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: "module",
+    sourceType: 'module',
   },
   overrides: [
     {
-      files: ["packages/babel-plugin-ember-test-metadata/__tests__/**/*.js"],
+      files: ['packages/babel-plugin-ember-test-metadata/__tests__/**/*.js'],
       env: {
         jest: true,
       },
