f183deb5c1b835a3cff58a3b208bff1934fda110
Glenn Cueto <glenn@gcgrafix.com>
2021-09-17 19:55:27 -0700

    fix plugin tests, all but embroider scenario

--- packages/babel-plugin-ember-test-metadata/__tests__/utils-test.js
+++ packages/babel-plugin-ember-test-metadata/__tests__/utils-test.js
@@ -2,7 +2,6 @@ const path = require('path');
 const {
   getNodeProperty,
   getNormalizedFilePath,
-  _getParsedClassicFilepath,
 } = require('../utils');
 
 describe('getNodeProperty', () => {
@@ -31,43 +30,6 @@ describe('getNodeProperty', () => {
 });
 
 describe('getNormalizedFilePath', () => {
-  const fileOpts = {
-    embroiderBuildPath: {
-      root: '',
-      filename: path.join(
-        'private',
-        'var',
-        'folders',
-        'abcdefg1234',
-        'T',
-        'embroider',
-        '098765',
-        'tests',
-        'acceptance',
-        'my-test.js'
-      ),
-    },
-    embroiderBuildPathTwoEmbroiderPathSegments: {
-      root: '',
-      filename: path.join(
-        'private',
-        'var',
-        'folders',
-        'embroider',
-        'abcdefg1234',
-        'T',
-        'embroider',
-        '098765',
-        'tests',
-        'acceptance',
-        'my-test.js'
-      ),
-    },
-    normalFilePath: {
-      root: '',
-      filename: path.join('this', 'is', 'not-an-embroider', 'path'),
-    },
-  };
   const projectConfiguration = {
     pkg: {
       name: 'test-app',
@@ -76,44 +38,38 @@ describe('getNormalizedFilePath', () => {
   };
 
   it('returns stripped file path as expected', () => {
-    expect(getNormalizedFilePath(fileOpts.embroiderBuildPath, projectConfiguration)).toBe(
+    expect(getNormalizedFilePath({
+      root: path.join(
+        'private',
+        'var',
+        'folders',
+        'abcdefg1234',
+        'T',
+        '1357'
+      ),
+      filename: path.join(
+        'private',
+        'var',
+        'folders',
+        'abcdefg1234',
+        'T',
+        'embroider',
+        '098765',
+        'tests',
+        'acceptance',
+        'my-test.js'
+      ),
+    }, projectConfiguration)).toBe(
       path.join('tests', 'acceptance', 'my-test.js')
     );
-    expect(
-      getNormalizedFilePath(
-        fileOpts.embroiderBuildPathTwoEmbroiderPathSegments,
-        projectConfiguration
-      )
-    ).toBe(path.join('tests', 'acceptance', 'my-test.js'));
   });
 
   it('returns unmodified file path when path does not include "embroider" as a segment', () => {
-    expect(getNormalizedFilePath(fileOpts.normalFilePath, projectConfiguration)).toBe(
+    expect(getNormalizedFilePath({
+      root: path.join('packages', 'test-app'),
+      filename: path.join('packages', 'test-app', 'this', 'is', 'not-an-embroider', 'path'),
+    }, projectConfiguration)).toBe(
       path.join('this', 'is', 'not-an-embroider', 'path')
     );
-  });
-});
-
-describe('_getParsedClassicFilepath', () => {
-  it('returns file path from classic build correctly', () => {
-    expect(
-      _getParsedClassicFilepath(['test-app', 'tests', 'acceptance', 'my-test.js'], {
-        pkg: {
-          name: 'test-app',
-          'ember-addon': {},
-        },
-      })
-    ).toBe(path.join('tests', 'acceptance', 'my-test.js'));
-    expect(
-      _getParsedClassicFilepath(
-        ['@parent-repo-name', 'test-app', 'tests', 'acceptance', 'my-test.js'],
-        {
-          pkg: {
-            name: `@parent-repo-name${path.sep}test-app`,
-            'ember-addon': {},
-          },
-        }
-      )
-    ).toBe(path.join('tests', 'acceptance', 'my-test.js'));
   });
 });
--- packages/babel-plugin-ember-test-metadata/utils.js
+++ packages/babel-plugin-ember-test-metadata/utils.js
@@ -69,72 +69,49 @@ function _addonFound(filename, projectConfiguration) {
   return pathSegments.find((addonName) => addonNames.includes(addonName));
 }
 
-function _getParsedAddonPathSegments(pathSegments, addonName) {
-  const RELATIVE_ADDON_PATH_ROOT = 1;
-
-  pathSegments[0] = 'lib';
-  pathSegments.splice(pathSegments.lastIndexOf(addonName) + RELATIVE_ADDON_PATH_ROOT, 0, 'tests');
-
-  return pathSegments;
-}
-
-function _getParsedClassicFilepath(pathSegments, projectConfiguration) {
-  const projectNamePathSeparators = projectConfiguration.pkg.name.split(path.sep);
-  const addonName = _addonFound(pathSegments, projectConfiguration);
-
-  pathSegments.splice(
-    0,
-    pathSegments.indexOf(projectNamePathSeparators[0]) + projectNamePathSeparators.length
-  );
-
-  if (addonName) {
-    pathSegments = _getParsedAddonPathSegments(pathSegments, addonName);
-  }
-
-  return pathSegments.join(path.sep);
-}
-
 /**
  * Get a normalized file path, based on whether the app build is classic or with Embroider
- * @param {object} fileOpts Babel state.file.opts which include root and filename props
+ * @param {object} {root, filename} Babel state.file.opts root & filename
  * @param {object} projectConfiguration Contains project name, ember-addon path info
  * @returns {string} E.g. tests/acceptance/my-test.js
  */
 function getNormalizedFilePath({ root, filename }, projectConfiguration) {
-  const pathSegments = {
-    projectPathPrefix: projectConfiguration.pkg.name,
-    basePath: path.parse(root).dir,
-    basePathWithTmp: path.join(path.parse(root).dir, path.parse(root).base),
-  };
+  const projectPathPrefix = projectConfiguration.pkg.name;
+  const rootDirWithBase = path.join(path.parse(root).dir, path.parse(root).base);
   const embroiderRegex = new RegExp(`embroider(${path.sep})(.{6})`);
   const embroiderPrefix = filename.match(embroiderRegex);
+  const embroiderPrefixIndex = filename.search(embroiderRegex);
   const addonFound = _addonFound(filename, projectConfiguration);
 
   if (embroiderPrefix) {
-    let embroiderBasePath = path.join(pathSegments.basePath, embroiderPrefix[0]);
+    let embroiderPath = filename.substring(embroiderPrefixIndex);
+    embroiderPath = embroiderPath.replace(embroiderPrefix[0] + path.sep, '');
 
-    if (filename.includes(pathSegments.basePathWithTmp)) {
-      embroiderBasePath = path.join(embroiderBasePath, pathSegments.basePathWithTmp);
+    if (filename.includes(rootDirWithBase)) {
+      embroiderPath = embroiderPath.replace(rootDirWithBase, '')
     }
 
-    return path.relative(embroiderBasePath, filename);
+    return embroiderPath;
   }
 
   if (addonFound) {
-    const basePathWithProjectPrefix = path.join(pathSegments.basePathWithTmp, pathSegments.projectPathPrefix);
-    const baseAddonPath = path.join(basePathWithProjectPrefix, 'tests', addonFound);
-    const targetAddonPath = path.join(basePathWithProjectPrefix, 'lib', addonFound, 'tests');
-    const parsedAddonPath = filename.replace(baseAddonPath, targetAddonPath);
+    const rootDirBaseWithProjectPrefix = path.join(rootDirWithBase, projectPathPrefix);
+    const sourceAddonPath = path.join(rootDirBaseWithProjectPrefix, 'tests', addonFound);
+    const targetAddonPath = path.join(rootDirBaseWithProjectPrefix, 'lib', addonFound, 'tests');
+    const parsedAddonPath = filename.replace(sourceAddonPath, targetAddonPath);
 
-    return parsedAddonPath.replace(basePathWithProjectPrefix + path.sep, '');
+    return parsedAddonPath.replace(rootDirBaseWithProjectPrefix + path.sep, '');
   }
 
-  return path.relative(path.join(root, pathSegments.projectPathPrefix), filename);
+  if (!root.includes(projectPathPrefix)) {
+    root = path.join(root, projectPathPrefix);
+  }
+
+  return path.relative(root, filename);
 }
 
 module.exports = {
   getNodeProperty,
   getNormalizedFilePath,
   getProjectConfiguration,
-  _getParsedClassicFilepath,
 };
