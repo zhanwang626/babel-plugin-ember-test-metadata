81ff1beb3e93650f58313ac65039dbb927ac2084
Cameron Cundiff <ccundiff@linkedin.com>
2021-09-21 15:26:08 -0400

    Delete unused tests

--- packages/test-scenarios/__tests__/scenario-test.js
+++ packages/test-scenarios/__tests__/scenario-test.js
@@ -45,56 +45,6 @@ async function classicInRepoAddon(project) {
   await addInRepoAddon(project, 'fake-addon');
 }
 
-async function embroider(project) {
-  project.linkDependency('@embroider/core', {
-    baseDir: __dirname,
-  });
-  project.linkDependency('@embroider/compat', {
-    baseDir: __dirname,
-  });
-  project.linkDependency('@embroider/webpack', {
-    baseDir: __dirname,
-  });
-  project.linkDependency('webpack', {
-    baseDir: __dirname,
-  });
-
-  merge(project.files, {
-    'ember-cli-build.js': `'use strict';
-
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-
-module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    babel: {
-      plugins: [
-        [
-          require.resolve('babel-plugin-ember-test-metadata'),
-          { enabled: true }
-        ]
-      ],
-    }
-  });
-
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack);
-};
-`,
-    tests: {
-      unit: getTestFiles(
-        'with-hooks-assert-includes-test.js',
-        'without-hooks-assert-includes-test.js',
-        'with-multiple-modules-assert-includes-test.js'
-      ),
-    },
-  });
-}
-
-async function embroiderInRepoAddon(project) {
-  await embroider(project);
-  await addInRepoAddon(project, 'fake-addon');
-}
-
 async function addInRepoAddon(project, name, version = '0.0.0') {
   project.linkDependency('ember-add-in-repo-tests', {
     baseDir: __dirname,
@@ -145,8 +95,6 @@ Scenarios.fromProject(baseApp)
   .expand({
     classic,
     classicInRepoAddon,
-    embroider,
-    embroiderInRepoAddon,
   })
   .map('app scenarios', (project) => {
     project.linkDependency('babel-plugin-ember-test-metadata', {
