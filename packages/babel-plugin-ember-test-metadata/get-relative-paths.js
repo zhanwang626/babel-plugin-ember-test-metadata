f4fdf46362cab00739f059bfbdc6cced965ea115
Steve Calvert <scalvert@linkedin.com>
2021-09-24 10:52:12 -0700

    Removing unnecessary param

--- packages/babel-plugin-ember-test-metadata/get-relative-paths.js
+++ packages/babel-plugin-ember-test-metadata/get-relative-paths.js
@@ -1,30 +1,30 @@
 const path = require('path');
 
-function _getRelativePathForClassic(preformattedPath, projectName) {
-  const formattedPath = preformattedPath
-    .slice(preformattedPath.lastIndexOf(projectName), preformattedPath.length)
+function _getRelativePathForClassic(filePath, projectName) {
+  const formattedPath = filePath
+    .slice(filePath.lastIndexOf(projectName), filePath.length)
     .replace(`${projectName}/`, '');
 
   return formattedPath;
 }
 
-function _getRelativePathForClassicInRepo(preformattedPath) {
-  const pathSegments = preformattedPath.split(path.sep);
+function _getRelativePathForClassicInRepo(filePath) {
+  const pathSegments = filePath.split(path.sep);
 
   return pathSegments
     .splice(pathSegments.indexOf('ember-add-in-repo-tests') + 1, pathSegments.length)
     .join(path.sep);
 }
 
-function _getRelativePathForEmbroider(preformattedPath) {
-  const pathSegments = preformattedPath.split(path.sep);
+function _getRelativePathForEmbroider(filePath) {
+  const pathSegments = filePath.split(path.sep);
   return pathSegments
     .splice(pathSegments.indexOf('embroider') + 2, pathSegments.length)
     .join(path.sep);
 }
 
-function _getRelativePathForEmbroiderInRepo(preformattedPath) {
-  return _getRelativePathForClassicInRepo(preformattedPath);
+function _getRelativePathForEmbroiderInRepo(filePath) {
+  return _getRelativePathForClassicInRepo(filePath);
 }
 
 module.exports = {
